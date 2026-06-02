ba1c6934abd5df32c8a2d64fe5d104ec0ed6a349
Augustin Trancart <augustin.trancart@oslandia.com>
2026-06-02 16:13:15 +0200

    Add a ctrl+p shortcut to open files

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -71,6 +69,14 @@ return {
             )
           end,
           desc = "Close buffer from tabline",
+        },
+        ["<C-p>"] = {
+          function()
+            require("snacks").picker.files {
+              hidden = vim.tbl_get((vim.uv or vim.loop).fs_stat ".git" or {}, "type") == "directory",
+            }
+          end,
+          -- desc = "Find files",
         },
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
